6054115ded5d5c823a87621dba5219c7c17da767
Alexia Ketllyn <90806201+alexiaketllyn@users.noreply.github.com>
2022-02-23 11:43:45 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,17 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Retornos
-        System.out.println("Exercício retornos");
-
-        double areaQuadrado = Quadrilatero.area(3);
-        System.out.println("Área do quadrado:" + areaQuadrado);
-
-        double areaRetangulo = Quadrilatero.area(5, 5);
-        System.out.println("Área do retangulo:" + areaRetangulo);
-
-        double areaTrapezio = Quadrilatero.area(7, 8, 9);
-        System.out.println("Área do trapezio:" + areaTrapezio);
-
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 5);
+        Quadrilatero.area(7, 8, 9 );
     }
 }
--- Quadrilatero.java
+++ Quadrilatero.java
@@ -2,18 +2,18 @@ package one.digitalinnovation.basecamp;
 
 public class Quadrilatero {
 
-    public static double area(double lado) {
+    public static void area(double lado) {
 
-        return lado * lado;
+        System.out.println("Área do quadrado:" + lado * lado);
     }
 
-    public static double area(double lado1, double lado2) {
+    public static void area(double lado1, double lado2) {
 
-        return lado1 * lado2;
+        System.out.println("Área do retângulo:" + lado1 * lado2);
     }
 
-    public static double area(double baseMaior, double baseMenor, double altura) {
+    public static void area(double baseMaior, double baseMenor, double altura) {
 
-        return ((baseMaior+baseMenor)*altura) / 2;
+        System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
     }
 }
