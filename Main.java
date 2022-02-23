a7c305e99776ea2b27bd27217669dcfe86ca0ca6
Alexia Ketllyn <90806201+alexiaketllyn@users.noreply.github.com>
2022-02-23 11:45:41 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,10 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Quadrilátero
-        System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5, 5);
-        Quadrilatero.area(7, 8, 9 );
+        // Retornos
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado:" + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5, 5);
+        System.out.println("Área do retangulo:" + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7, 8, 9);
+        System.out.println("Área do trapezio:" + areaTrapezio);
+
     }
 }
